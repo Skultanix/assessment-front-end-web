ed134c513f8d4d76330f702c4cc900e22d5c1154
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-25 11:03:17 -0700

    Image mouseover compliment added. Part 2 complete.

--- about.js
+++ about.js
@@ -12,3 +12,13 @@ function handleSubmit(evt) {
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function duckComplement(evt) {
+	evt.preventDefault()
+
+	alert("You have aged better than this duck.")
+}
+
+let duck = document.querySelector('img')
+
+duck.addEventListener('mouseover', duckComplement)
