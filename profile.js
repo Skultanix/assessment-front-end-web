54fec5c5f68eddd0e211460534e78294cbb3d233
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-25 15:37:18 -0700

    Part 4 complete.

---
+++ profile.js
@@ -0,0 +1,13 @@
+function favColor() {
+    return alert("My favorite color is purple.")
+}
+
+document.getElementById('color').addEventListener('click',favColor)
+function favPlace() {
+    return alert("My favorite place is on a mountain, esp. Mt. Diablo.")
+}
+document.getElementById('place').addEventListener('click',favPlace)
+function favRit() {
+    return alert("My favorite ritual is finishing the last set on deadlifts.")
+}
+document.getElementById('ritual').addEventListener('click',favRit)
